19a696cd6b0314ee64fcb3e94e4ebefd420f8fd8
hp <namstudionsk@gmail.com>
2019-01-21 14:40:31 +0700

    wip

--- src/main/java/com/nam/spring_44/dao/UserDao.java
+++ src/main/java/com/nam/spring_44/dao/UserDao.java
@@ -2,44 +2,144 @@ package com.nam.spring_44.dao;
 
 import com.nam.spring_44.bean.User;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
 
-    private static List<User> list = new ArrayList<>();
+    public static Connection getConnection(){
+        Connection con = null;
 
-    public static int save(User u){
-        return 42;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/crud_test", "testuser", "5550635");
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return con;
     }
 
-    public static List<User> getAllRecords(){
+    public static int save(User u){
 
+        int status = 0;
 
-        User u = new User();
+        try{
 
-        u.setId(42);
-        u.setName("Alex");
-        u.setPassword("qwerty");
-        u.setEmail("1@2");
-        u.setSex("male");
-        u.setCountry("Belarus");
+            Connection con = getConnection();
 
-        list.add(u);
+            PreparedStatement ps = con.prepareStatement("insert into register(name, password, email, sex, country) values(?,?,?,?,?)");
 
-        return list;
+            ps.setString(1, u.getName());
+            ps.setString(2, u.getPassword());
+            ps.setString(3, u.getEmail());
+            ps.setString(4, u.getSex());
+            ps.setString(5, u.getCountry());
+
+            status = ps.executeUpdate();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return status;
     }
 
     public static int update(User u){
-        return 42;
+        int status = 0;
+
+        try{
+
+            Connection con = getConnection();
+
+            PreparedStatement ps = con.prepareStatement("update register set name=?, password=?, email=?, sex=?, country=? where id=?");
+
+            ps.setString(1, u.getName());
+            ps.setString(2, u.getPassword());
+            ps.setString(3, u.getEmail());
+            ps.setString(4, u.getSex());
+            ps.setString(5, u.getCountry());
+            ps.setInt(6, u.getId());
+
+            status = ps.executeUpdate();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return status;
+
     }
 
     public static void delete(User u){
-        //return 42;
+        int status = 0;
+
+        try{
+            Connection con = getConnection();
+
+            PreparedStatement ps=con.prepareStatement("delete from register where id=?");
+
+            ps.setInt(1, u.getId());
+
+            status = ps.executeUpdate();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
+
+    public static List<User> getAllRecords(){
+        List<User> list = new ArrayList<>();
+
+        try{
+            Connection con = getConnection();
+            PreparedStatement ps = con.prepareStatement("select * from register");
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                User u = new User();
+
+                u.setId(rs.getInt("id"));
+                u.setName(rs.getString("name"));
+                u.setPassword(rs.getString("password"));
+                u.setEmail(rs.getString("email"));
+                u.setSex(rs.getString("sex"));
+                u.setCountry(rs.getString("country"));
+
+                list.add(u);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 
     public static User getRecordById(int id){
-        return list.get(0);
-    }
+        User u = null;
 
+        try{
+            Connection con = getConnection();
+            PreparedStatement ps = con.prepareStatement("select * from register where id=?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                u = new User();
+
+                u.setId(rs.getInt("id"));
+                u.setName(rs.getString("name"));
+                u.setPassword(rs.getString("password"));
+                u.setEmail(rs.getString("email"));
+                u.setSex(rs.getString("sex"));
+                u.setCountry(rs.getString("country"));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return u;
+    }
 }
